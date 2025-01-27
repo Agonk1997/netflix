945a3579bce3079a172e7db636034b20e59862c2
Gili <gyltene.jusufi@student.cacttus.education>
2025-01-27 14:45:44 +0100

    CategoryDonex2

---
+++ src/main/java/com/example/netflix/MovieCategory.java
@@ -0,0 +1,15 @@
+package com.example.netflix;
+
+
+public enum MovieCategory {
+    ACTION,
+    COMEDY,
+    DRAMA,
+    HORROR,
+    SCI_FI,
+    DOCUMENTARY,
+    ANIMATION,
+    THRILLER,
+    ROMANCE,
+    ADVENTURE
+}
